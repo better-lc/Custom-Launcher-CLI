a957e963c08608d437fd6e8d370b17cb10bfe44c
Aetopia <41850963+Aetopia@users.noreply.github.com>
2021-10-03 20:00:45 +0530

    Update parse.py

--- parse.py
+++ parse.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.insert(1, 'modules')
 import website
+Finish=False
 def input(command):
 #Input Parsing
     try:
@@ -17,33 +18,38 @@ def input(command):
     if cmd.lower()=="search" or cmd.lower()=="s":
         try:
             website.search(argument1, argument2)
+            Finish=True
         except:
-            print("search/s <engine: Google/DuckDuckGo> <query>")
+            print("search/s <engine: Google/DuckDuckGo/DuckDuckGoLite/QWant/Brave> <query>")
     elif cmd.lower()=="wikipedia" or cmd.lower()=="wk":
         try:
             website.wikipedia(argument)
+            Finish=True
         except:
+            time.sleep()
             print("wikipedia/wk <query>")
     elif cmd.lower()=="namemc" or cmd.lower()=="n":
         try:
             website.namemc(argument)
+            Finish=True
         except:
             print("namemc/n <query>")
     elif cmd.lower()=="github" or cmd.lower()=="gh":
         try:
             website.github(argument)
+            Finish=True
         except:
             print("github/gh <user/repository")
     elif cmd.lower()=="translate" or cmd.lower()=="t":
         try:
             website.translate(argument1,argument2,argument3)
+            Finish=True
         except:
             print("translate/t <language from> <language to> <text>")
     elif cmd.lower()=="chocopackage" or cmd.lower()=="cp":
         try:
             website.chocopackagelookup(argument)
-        except OSError as Error:
-            print("chocopackage/cp <package name>")
-                 
+        except:
+            print("chocopackage/cp <package name>")               
     else:
         print("Invaild Command/Syntax.")
