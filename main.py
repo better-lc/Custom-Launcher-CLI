c0efb25f1d6062900f363a388cb88f00d0892ed9
Aetopia <41850963+Aetopia@users.noreply.github.com>
2021-10-03 21:42:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 #Modules
 import sys
+import os
+import platform
 sys.path.insert(1, 'modules')
 import parse
 
+#Title
+Version="v0.1 Alpha"
+if platform.system=="Windows":
+    ctypes.windll.kernel32.SetConsoleTitleW("Custom Launcher "+ Version)
+else:
+    sys.stdout.write("Custom Launcher "+Version)
 
 #Initialization
 Exit=False
@@ -14,6 +22,7 @@ print(" ██║░░██╗██║░░░░░")
 print(" ╚█████╔╝███████╗")
 print(" ░╚════╝░╚══════╝")
 print("")
+print("Version:",Version)
 print("Custom Launcher Python Rewrite by Aetopia.")
 #Main
 while Exit==False:
